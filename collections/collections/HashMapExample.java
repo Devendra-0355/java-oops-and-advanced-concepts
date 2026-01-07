a21ff38cc073bb6105af81ca18e3d07f06797ae7
Arnepalli Devendra Naidu <103933198+Devendra-0355@users.noreply.github.com>
2026-01-07 11:59:44 +0530

    Create HashMapExample.java

---
+++ collections/collections/HashMapExample.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Java");
+        map.put(2, "Python");
+        System.out.println(map);
+    }
+}
